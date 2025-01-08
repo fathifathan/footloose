e21771347a4b7020b87e2ae8311c195899f3d4f6
Fathi Fathan <88713515+fathifathan@users.noreply.github.com>
2025-01-09 00:47:33 +0700

    Update script.js
    update loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+window.addEventListener("load", () =>{
+  const loader = document.querySelector(".loading-page");
+
+  loader.classList.add("loading-page-hidden");
+
+  loader.addEventListener("transitionend", () => {
+    window.location.href = "index.html";
+  })
+})
+
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.8,
+  }
+);
+
 // init Isotope
 var $grid = $('.collection-list').isotope({
   // options
